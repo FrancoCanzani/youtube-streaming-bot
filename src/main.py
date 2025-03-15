a7237c43477d105b34db4f92f067bdb68d021eb4
Franco Canzani <francocanzani@gmail.com>
2025-03-15 16:53:58 +0100

    init

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from helpers.helpers import get_channel_subscribers, get_total_views_from_datetime
+from datetime import datetime, timedelta
+
+one_week_ago = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%dT%H:%M:%SZ')
+one_month_ago = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+api_key = "AIzaSyBYpUV9qy0LnizDcjjrXbH6oVvLaeO7LHo"
+channel_id = "UCvCTWHCbBC0b9UIeLeNs8ug"
+
+def main():
+    get_channel_subscribers(api_key, channel_id)
+    get_total_views_from_datetime(api_key, channel_id, one_month_ago)
+    
+if __name__ == "__main__":
+    main()
